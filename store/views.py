c8a6dbd9dc8666c94162bde54cbb61aa9cf1ca29
Athulya <athulyabalakrishnanv@gmail.com>
2022-07-30 21:47:56 +0530

    Second Commit

--- store/views.py
+++ store/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models.product import Product
+from .models.category import Category
+from .models.customer import Customer
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    products = None
+    categories = Category.get_all_categories()
+    # print(products)
+    # print(request.GET)
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products();
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request,'index.html', data)
+
+def signup(request):
+    # print(request.method)
+    if request.method == 'GET':
+        return render(request,'signup.html')
+    else:
+        postData = request.POST
+        first_name = postData.get('firstname')
+        last_name = postData.get('lastname')
+        phone = postData.get('phone')
+        email = postData.get('email')
+        password = postData.get('password')
+        customer = Customer(first_name=first_name,last_name=last_name,phone=phone,email=email,password=password)
+        customer.register()
+        return HttpResponse('success')
+        # return HttpResponse('request.POST') middleware "request.POST.get('email')""
