14cd150d8470a1515e9b4bc9c48029faf71dc8c7
Athulya <athulyabalakrishnanv@gmail.com>
2022-08-01 13:24:23 +0530

    Fourth Commit

---
+++ store/templatetags/cart.py
@@ -0,0 +1,26 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='is_in_cart')
+def is_i_cart(product,cart):
+    keys = cart.keys()
+    for id in keys:
+        print(id,product.id)
+        # print(type(id),type(product.id))
+
+        if int(id) == product.id:
+            return True
+    # print(keys)
+    print(product,cart)
+    return False
+
+@register.filter(name='cart_quantity')
+def cart_quantity(product,cart):
+    keys = cart.keys()
+    for id in keys:
+
+        if int(id) == product.id:
+            return cart.get(id)
+    
+    return 0
